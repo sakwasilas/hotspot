9b0ee5332bec13b188c8ce2550576052f9b52816
sakwasilas <sakwahsilas@gmail.com>
2026-04-22 02:04:03 -0700

    good one

--- create.py
+++ create.py
@@ -8,10 +8,12 @@ Base.metadata.create_all(bind=engine)
 db = SessionLocal()
 
 packages = [
-    {"name": "1 hour", "price": 20, "duration_hours": 1},
-    {"name": "3 hours", "price": 25, "duration_hours": 3},
-    {"name": "8 hours", "price": 40, "duration_hours": 8},
-    {"name": "15 hours", "price": 100, "duration_hours": 15},
+    {"name": "1 hour", "price": 10, "duration_hours": 1},
+    {"name": "2 hour", "price": 20, "duration_hours": 1},
+    {"name": "3 hour", "price": 30, "duration_hours": 1},
+    {"name": "5 hours", "price": 50, "duration_hours": 3},
+    {"name": "8 hours", "price": 80, "duration_hours": 8},
+    {"name": "15 hours", "price": 150, "duration_hours": 15},
 ]
 
 for pkg in packages:
