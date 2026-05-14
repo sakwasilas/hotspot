de095b86b02bdaf0a0fc52cf259afb2233f4a3b8
sakwasilas <sakwahsilas@gmail.com>
2026-05-14 16:02:30 +0300

    changed to postgresql

--- connections.py
+++ connections.py
@@ -1,10 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+import os
 
-#DATABASE_URL = "mysql+pymysql://root:2480@localhost/hotspot_2_kevin"
-DATABASE_URL = "sqlite:///hotspot_2_kevin"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://hotspot_1xun_user:I3uqYdRFlWePIJ7HdKlId9TvTKcv42Td@dpg-d82sc33bc2fs73bglrr0-a.oregon-postgres.render.com/hotspot_1xun"
+)
 
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=10,
+    max_overflow=20
+)
 
 Session = scoped_session(sessionmaker(bind=engine))
 SessionLocal = Session
